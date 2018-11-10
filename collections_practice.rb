423063d8bdb63869e0063d6cacc99fd017eb0393
John Mark Redding <johnmarkredding@gmail.com>
2018-11-10 22:16:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,28 @@
 # your code goes here
+def begins_with_r(tools)
+  false_arr = tools.select { |x|
+    !(x.start_with?("r"))
+  }
+  false_arr.empty?
+end
+
+def contain_a(arr)
+  arr.select {|x|
+    x.include? "a"
+  }
+end
+
+def first_wa(arr)
+  new_arr = arr.select {|x|
+    x.to_s.start_with?("wa")
+  }
+  new_arr.first
+end
+
+def remove_non_strings(arr)
+  arr.delete_if {|x|
+    x.is_a?(String)
+  }
+end
+
+remove_non_strings(["arrr", 445, "64"])
